8c50caac27e41cdce0d5722a397aa464d8419da1
Md Nazmul Hasan <farhapartex@gmail.com>
2020-05-01 22:28:59 +0600

    time change functionality is added

--- pihrbot/files.py
+++ pihrbot/files.py
@@ -6,9 +6,6 @@ import os.path
 CURRENT_DIR = os.getcwd()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def do():
-    print()
-
 def check_file_exists():
     return True if Path(BASE_DIR +"/credentials.txt").is_file() else False
 
@@ -22,21 +19,50 @@ def create_and_set_credentials(username, password):
     except:
         return False
 
+
 def get_credentials():
     with open(BASE_DIR + "/credentials.txt", "r") as user_file:
         username, password = [ch.replace("\n", "") for ch in user_file.readlines()]
         user_file.close()
         return (username, password)
 
+
 def get_time():
     with open(BASE_DIR + "/pihrbot/timer.txt", "r") as times:
         in_time, out_time = [ch.replace("\n", "") for ch in times.readlines()]
         times.close()
-        return (in_time, out_time)
+    in_time = in_time.split()
+    out_time = out_time.split()
+
+    if in_time[1] == "am" or in_time[1] == "AM":
+        in_time = in_time[0]
+    elif in_time[1] == "pm" or in_time[1] == "PM":
+        in_time = in_time[0].split(":")
+        in_time =  str(int(in_time[0])+12) + ":" + in_time[1]
+    
+    if out_time[1] == "am" or out_time[1] == "AM":
+        out_time = out_time[0]
+    elif out_time[1] == "pm" or out_time[1] == "PM":
+        out_time = out_time[0].split(":")
+        out_time =  str(int(out_time[0])+12) + ":" + out_time[1]
+
+    return (in_time, out_time)
+
+
+def change_time(in_time, out_time):
+    try:
+        with open(BASE_DIR + "/pihrbot/timer.txt", "r+") as times:
+            times.seek(0)
+            times.write(in_time+ "\n")
+            times.write(out_time+ "\n")
+            times.close()
+        return True
+    except:
+        return False
+
 
 def check_driver_exists():
     try:
         return True if Path(BASE_DIR +"/driver/chromedriver").is_file() else False
     except:
         return False
-
--- setting/settings.py
+++ setting/settings.py
@@ -7,7 +7,7 @@ from pihrbot.install import *
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def check_args(args):
-    if len(args) <=1 or (args[0] != "init.py" or args[1] != "run"):
+    if len(args) <=1:
         print("Command is wrong. Run python3 init.py run")
     elif args[0] == "init.py" and args[1] == "run":
         try:
@@ -16,5 +16,13 @@ def check_args(args):
                 celery = os.system("celery -A task worker --beat --loglevel=info")
             else:
                 install = Install()
+        except:
+            print("It seems there are some problems. Try again!")
+    
+    elif args[0] == "init.py" and args[1] == "timechange":
+        try:
+            in_time = input("Type in time (format: hh:mm am/pm) ")
+            out_time = input("Type out time (format: hh:mm am/pm) ")
+            change_time(in_time, out_time)
         except:
             print("It seems there are some problems. Try again!")
